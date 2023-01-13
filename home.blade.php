dd2198bd567fd0ae97a2be919eadbe157c4b369c
syahfirman2106 <122583585+syahfirman2106@users.noreply.github.com>
2023-01-13 19:53:15 +0700

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="banner">
+        <div class="navbar">
+            <h1 class="logo">Home Ralavel</h1>
+            <ul>
+                <li><a href="">Beranda</a></li>
+                <li><a href="">Keranjang</a></li>
+                <li><a href="">Kontak</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>SELAMAT DATANG</h1>
+            <div>
+                <button type="button"><span></span>Login</button>
+                <button type="button"><span></span>Profil</button>
+            </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
